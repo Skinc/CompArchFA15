cc720b5efec69054c498a250296033466f26bb68
unknown <sskikne@8SZJ6R1.olin.edu>
2015-10-27 20:33:35 -0400

    initial work on register

--- HW/HW4/register.v
+++ HW/HW4/register.v
@@ -15,3 +15,52 @@ input		clk
     end
 
 endmodule
+
+
+module register32
+(
+output reg[31:0]	q,
+input[31:0]		d,
+input		wrenable,
+input		clk
+);
+integer i;
+	for (i = 0; i < 32; i = i+1 ) begin
+    	always @(posedge clk) begin
+    		if(wrenable) begin
+    	
+    			q[i] = d[i];
+    
+    		end
+        end
+    end
+
+endmodule
+
+module register32zerp
+(
+output reg[31:0]	q,
+input[31:0]		d,
+input		wrenable,
+input		clk
+);
+integer i;
+	for (i = 0; i < 32; i = i+1 ) begin
+    	q[i] = 0;
+    end
+
+endmodule
+
+
+
+
+module mux32to1by1
+(
+	output      out,
+	input[4:0]  address,
+	input[31:0] inputs
+);
+
+out = inputs[address]
+
+endmodule
